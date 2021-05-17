79ce2576ebc6a01dfe32948e41ac905b7d03190a
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2021-05-17 12:11:06 +0530

    fixed web3 not found issue

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Contract } from "@ethersproject/contracts";
 import { getDefaultProvider } from "@ethersproject/providers";
 import { useQuery } from "@apollo/react-hooks";
 
@@ -11,17 +10,9 @@ import GET_TRANSFERS from "./graphql/subgraph";
 import { AccordionItemHeading } from 'react-accessible-accordion';
 import 'react-accessible-accordion/dist/fancy-example.css';
 import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
+import Web3 from "web3";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
-}
+
 async function getAirdropContract() {
   const defaultProvider = getDefaultProvider();
   const web3 = new Web3(defaultProvider)
