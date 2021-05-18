8215bfecfe6a80ffbe396afc42fe1453753f897b
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2021-05-18 17:53:55 +0530

    feat: fix content

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   erc20: "0x4321754eb0D9942e9b961841c51F52c3f5Dfa240",
-  airdrop: "0xE8D9ec9ea6AeBa76975301741cCB496f82FE83f5"
+  airdrop: "0x28Cca76f6e8eC81e4550ecd761f899110b060E97"
 };
 
 export default addresses;
